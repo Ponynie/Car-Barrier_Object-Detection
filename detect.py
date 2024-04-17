24945c07b425f30ae05be3bcc222e89bc26a2e5e
Phichaphop Raqsaboon <105978782+Ponynie@users.noreply.github.com>
2024-04-18 03:11:12 +0700

    model V2

--- detect.py
+++ detect.py
@@ -169,7 +169,7 @@ opt = None
 def run(source: str):
     #python detect.py --conf 0.25 --source     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp5/weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp6/weights/best.pt', help='model.pt path(s)')
     parser.add_argument('--source', type=str, default=source, help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
